411b60b3667b4da0415a16511e5e23254f606f53
Patrick Rachford <prachford@icloud.com>
2023-01-04 15:25:20 -0800

    Adds mock server

--- tests/fast_api/test_fast_route.py
+++ tests/fast_api/test_fast_route.py
@@ -1,11 +1,33 @@
+import pytest
 from fastapi.testclient import TestClient
+from temporalio.client import Client
 
 from fast_api.run_fast import app
 
 client = TestClient(app)
 
 
-def test_read_main():
+class ServerAddress:
+    def get_address(self) -> str:
+        raise NotImplementedError
+
+
+class RealServerAddress(ServerAddress):
+    def get_address(self) -> str:
+        return "localhost:7233"
+
+
+class MockServerAddress(ServerAddress):
+    def get_address(self) -> str:
+        return "mock_server:1234"
+
+
+@pytest.fixture
+def server_address():
+    return MockServerAddress()
+
+
+def test_read_main(server_address):
     response = client.get("/")
     assert response.status_code == 200
     assert response.json() == "Hello, World!"

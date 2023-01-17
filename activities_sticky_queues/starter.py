7e173bf219d898f23b140466605230d8fcfa7f32
Oracen <73895482+Oracen@users.noreply.github.com>
2023-01-17 20:59:21 +1000

    Update activities_sticky_queues/starter.py
    Co-authored-by: Chad Retz <chad.retz@gmail.com>

--- activities_sticky_queues/starter.py
+++ activities_sticky_queues/starter.py
@@ -14,7 +14,9 @@ async def main():
     futures = []
     for _ in range(10):
         handle = client.execute_workflow(
-            FileProcessing.run, id=str(uuid4()), task_queue="distribution-queue"
+            FileProcessing.run,
+            id=f"activity_sticky_queue-workflow-id-{i}",
+            task_queue="activity_sticky_queue-distribution-queue",
         )
         await asyncio.sleep(0.1)
         futures.append(handle)

57c70c0dc6b9c1899a42f97c944bef9c1848387e
NED <ned@vannevarlabs.com>
2023-01-23 13:03:06 -0800

    add workflow interceptor class with replay check & import passthrough

--- sentry/interceptor.py
+++ sentry/interceptor.py
@@ -1,13 +1,23 @@
 from dataclasses import asdict, is_dataclass
-from typing import Any
+from typing import Any, Optional, Type
 
-from sentry_sdk import Hub, capture_exception, set_context, set_tag
-from temporalio import activity
+from temporalio import activity, workflow
 from temporalio.worker import (
     ActivityInboundInterceptor,
     ExecuteActivityInput,
+    ExecuteWorkflowInput,
     Interceptor,
+    WorkflowInboundInterceptor,
+    WorkflowInterceptorClassInput,
 )
+
+with workflow.unsafe.imports_passed_through():
+    from sentry_sdk import Hub, capture_exception, set_context, set_tag
+
+
+def _set_common_workflow_tags(info: workflow.Info | activity.Info):
+    set_tag("temporal.workflow.type", info.workflow_type)
+    set_tag("temporal.workflow.id", info.workflow_id)
 
 
 class _SentryActivityInboundInterceptor(ActivityInboundInterceptor):
@@ -18,8 +28,7 @@ class _SentryActivityInboundInterceptor(ActivityInboundInterceptor):
             set_tag("module", input.fn.__module__ + "." + input.fn.__qualname__)
 
             activity_info = activity.info()
-            set_tag("temporal.workflow.type", activity_info.workflow_type)
-            set_tag("temporal.workflow.id", activity_info.workflow_id)
+            _set_common_workflow_tags(activity_info)
             set_tag("temporal.activity.id", activity_info.activity_id)
             set_tag("temporal.activity.type", activity_info.activity_type)
             set_tag("temporal.activity.task_queue", activity_info.task_queue)
@@ -35,6 +44,29 @@ class _SentryActivityInboundInterceptor(ActivityInboundInterceptor):
                 raise e
 
 
+class _SentryWorkflowInterceptor(WorkflowInboundInterceptor):
+    async def execute_workflow(self, input: ExecuteWorkflowInput) -> Any:
+        # https://docs.sentry.io/platforms/python/troubleshooting/#addressing-concurrency-issues
+        with Hub(Hub.current):
+            set_tag("temporal.execution_type", "workflow")
+            set_tag("module", input.fn.__module__ + "." + input.fn.__qualname__)
+            workflow_info = workflow.info()
+            _set_common_workflow_tags(workflow_info)
+            set_tag("temporal.workflow.task_queue", workflow_info.task_queue)
+            set_tag("temporal.workflow.namespace", workflow_info.namespace)
+            set_tag("temporal.workflow.run_id", workflow_info.run_id)
+            try:
+                return await super().execute_workflow(input)
+            except Exception as e:
+                if len(input.args) == 1 and is_dataclass(input.args[0]):
+                    set_context("temporal.workflow.input", asdict(input.args[0]))
+                set_context("temporal.workflow.info", workflow.info().__dict__)
+
+                if not workflow.unsafe.is_replaying():
+                    capture_exception()
+                raise e
+
+
 class SentryInterceptor(Interceptor):
     """Temporal Interceptor class which will report workflow & activity exceptions to Sentry"""
 
@@ -45,3 +77,8 @@ class SentryInterceptor(Interceptor):
         :py:meth:`temporalio.worker.Interceptor.intercept_activity`.
         """
         return _SentryActivityInboundInterceptor(super().intercept_activity(next))
+
+    def workflow_interceptor_class(
+        self, input: WorkflowInterceptorClassInput
+    ) -> Optional[Type[WorkflowInboundInterceptor]]:
+        return _SentryWorkflowInterceptor

9bd6d89d2a7d90ecf5a9b3ce2c8da94cee4f6692
ktenzer <keith.tenzer@gmail.com>
2023-02-28 19:36:41 -0800

    re-factored to utilize same workflow and make example of patching easier to follow
    Signed-off-by: ktenzer <keith.tenzer@gmail.com>

--- hello/hello_patch.py
+++ hello/hello_patch.py
@@ -25,15 +25,13 @@ async def compose_greeting(input: ComposeGreetingInput) -> str:
     return f"{input.greeting}, {input.name}!"
 
 
-# MyWorkflowDeployedFirst workflow is example of first version of workflow
-@workflow.defn
-class MyWorkflowDeployedFirst:
+# V1 of patch-workflow
+@workflow.defn(name="patch-workflow")
+class MyWorkflow:
     @workflow.run
     async def run(self, name: str) -> str:
-        print(f"Running MyWorkflowDeployedFirst workflow with parameter %s" % name)
-        workflow.logger.info(
-            "Running MyWorkflowDeployedFirst workflow with parameter %s" % name
-        )
+        print(f"Running patch-workflow with parameter %s" % name)
+        workflow.logger.info("Running patch-workflow with parameter %s" % name)
         greeting = await workflow.execute_activity(
             compose_greeting,
             ComposeGreetingInput("Hello", name),
@@ -42,16 +40,14 @@ class MyWorkflowDeployedFirst:
         return greeting
 
 
-# MyWorkflowPatched workflow is example of using patch to change the workflow for
-# newly started workflows without changing the behavior of existing workflows.
-@workflow.defn
+# V2 of patch-workflow using patched where we have changed newly started
+# workflow behavior without changing the behavior of currently running workflows
+@workflow.defn(name="patch-workflow")
 class MyWorkflowPatched:
     @workflow.run
     async def run(self, name: str) -> str:
-        print(f"Running MyWorkflowPatched workflow with parameter %s" % name)
-        workflow.logger.info(
-            "Running MyWorkflowPatched workflow with parameter %s" % name
-        )
+        print(f"Running patch-workflow with parameter %s" % name)
+        workflow.logger.info("Running patch-workflow with parameter %s" % name)
         if workflow.patched("my-patch-v2"):
             greeting = await workflow.execute_activity(
                 compose_greeting,
@@ -71,16 +67,14 @@ class MyWorkflowPatched:
             return greeting
 
 
-# MyWorkflowPatchDeprecated workflow is example for after no older workflows are
-# running and we want to deprecate the patch and just use the new version.
-@workflow.defn
+# V3 of patch-workflow using deprecate_patch where all the old V1 workflows
+# have completed, we no longer need to preserve V1 and now just have V2
+@workflow.defn(name="patch-worklow")
 class MyWorkflowPatchDeprecated:
     @workflow.run
     async def run(self, name: str) -> str:
-        print(f"Running MyWorkflowPatchDeprecated workflow with parameter %s" % name)
-        workflow.logger.info(
-            "Running MyWorkflowPatchDeprecated workflow with parameter %s" % name
-        )
+        print(f"Running patch-workflow with parameter %s" % name)
+        workflow.logger.info("Running patch-workflow with parameter %s" % name)
         workflow.deprecate_patch("my-patch-v2")
         greeting = await workflow.execute_activity(
             compose_greeting,
@@ -94,21 +88,15 @@ class MyWorkflowPatchDeprecated:
 
 
 async def main():
-    # Check arguments and ensure either v1, v2 or v3 is passed in as argument
+    # Check Args
     if len(sys.argv) > 2:
-        print(
-            f"Incorrect arguments: {sys.argv[0]} v1 or {sys.argv[0]} v2 or {sys.argv[0]} v3"
-        )
+        print(f"Incorrect arguments: {sys.argv[0]} v1|v2|v3")
         exit()
     if len(sys.argv) <= 1:
-        print(
-            f"Incorrect arguments: {sys.argv[0]} v1 or {sys.argv[0]} v2 or {sys.argv[0]} v3"
-        )
+        print(f"Incorrect arguments: {sys.argv[0]} v1|v2|v3v3")
         exit()
     if sys.argv[1] != "v1" and sys.argv[1] != "v2" and sys.argv[1] != "v3":
-        print(
-            f"Incorrect arguments: {sys.argv[0]} v1 or {sys.argv[0]} v2 or {sys.argv[0]} v3"
-        )
+        print(f"Incorrect arguments: {sys.argv[0]} v1|v2|v3")
         exit()
 
     version = sys.argv[1]
@@ -119,73 +107,42 @@ async def main():
     # Start client
     client = await Client.connect("localhost:7233")
 
-    # Run a worker for the workflow
+    # Set workflow_class to the proper class based on version
+    workflow_class = ""
+    if version == "v1":
+        workflow_class = MyWorkflow  # type: ignore
+    elif version == "v2":
+        workflow_class = MyWorkflowPatched  # type: ignore
+    elif version == "v3":
+        workflow_class = MyWorkflowPatchDeprecated  # type: ignore
+    else:
+        print(f"Incorrect arguments: {sys.argv[0]} v1|v2|v3")
+        exit()
+
+    # While the worker is running, use the client to run the workflow and
+    # print out its result. Check if the workflow is already running and
+    # if so wait for the existing run to complete. Note, in many production setups,
+    # the client would be in a completely separate process from the worker.
     async with Worker(
         client,
         task_queue="hello-patch-task-queue",
-        workflows=[
-            MyWorkflowDeployedFirst,
-            MyWorkflowPatched,
-            MyWorkflowPatchDeprecated,
-        ],
+        workflows=[workflow_class],  # type: ignore
         activities=[compose_greeting],
     ):
-        # While the worker is running, use the client to run the workflow and
-        # print out its result. A workflow will be chosen based on argument passed in.
-        # Check if the workflow is already running and if so wait for the
-        # existing run to complete. Note, in many production setups,
-        # the client would be in a completely separate process from the worker.
-
-        if version == "v1":
-            try:
-                result = await client.execute_workflow(
-                    MyWorkflowDeployedFirst.run,
-                    "World",
-                    id="hello-patch-workflow-id",
-                    task_queue="hello-patch-task-queue",
-                )
-                print(f"Result: {result}")
-            except exceptions.WorkflowAlreadyStartedError:
-                print(f"Workflow already running")
-                result = await client.get_workflow_handle(
-                    "hello-patch-workflow-id"
-                ).result()
-                print(f"Result: {result}")
-        elif version == "v2":
-            try:
-                result = await client.execute_workflow(
-                    MyWorkflowPatched.run,
-                    "World",
-                    id="hello-patch-workflow-id",
-                    task_queue="hello-patch-task-queue",
-                )
-                print(f"Result: {result}")
-            except exceptions.WorkflowAlreadyStartedError:
-                print(f"Workflow already running")
-                result = await client.get_workflow_handle(
-                    "hello-patch-workflow-id"
-                ).result()
-                print(f"Result: {result}")
-        elif version == "v3":
-            try:
-                result = await client.execute_workflow(
-                    MyWorkflowPatchDeprecated.run,
-                    "World",
-                    id="hello-patch-workflow-id",
-                    task_queue="hello-patch-task-queue",
-                )
-                print(f"Result: {result}")
-            except exceptions.WorkflowAlreadyStartedError:
-                print(f"Workflow already running")
-                result = await client.get_workflow_handle(
-                    "hello-patch-workflow-id"
-                ).result()
-                print(f"Result: {result}")
-        else:
-            print(
-                f"Incorrect arguments: {sys.argv[0]} v1 or {sys.argv[0]} v2 or {sys.argv[0]} v3"
+        try:
+            result = await client.execute_workflow(
+                workflow_class.run,  # type: ignore
+                "World",
+                id="hello-patch-workflow-id",
+                task_queue="hello-patch-task-queue",
             )
-            exit()
+            print(f"Result: {result}")
+        except exceptions.WorkflowAlreadyStartedError:
+            print(f"Workflow already running")
+            result = await client.get_workflow_handle(
+                "hello-patch-workflow-id"
+            ).result()
+            print(f"Result: {result}")
 
 
 if __name__ == "__main__":

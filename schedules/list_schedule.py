872f435e57d49c9d9c6be6eacbf7f6b44b56cb95
Patrick Rachford <prachford@icloud.com>
2023-03-27 10:04:39 -0700

    resolve comments

--- schedules/list_schedule.py
+++ schedules/list_schedule.py
@@ -1,18 +1,13 @@
 import asyncio
 
-from temporalio.client import Client, ScheduleListInfo
+from temporalio.client import Client
 
 
 async def main() -> None:
     client = await Client.connect("localhost:7233")
 
-    async def list_schedule_simple() -> None:
-        async for schedule in await client.list_schedules():
-            schedule_info = schedule.info
-            if isinstance(schedule_info, ScheduleListInfo):
-                print(f"List Schedule Info: {schedule_info}.")
-
-    return await list_schedule_simple()
+    async for schedule in await client.list_schedules():
+        print(f"List Schedule Info: {schedule.info}.")
 
 
 if __name__ == "__main__":

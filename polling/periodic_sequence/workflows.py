00740760bec9388a549d20437df6e05c2fd67495
Patrick Rachford <prachford@icloud.com>
2023-04-14 11:37:17 -0700

    updates code sample

--- polling/periodic_sequence/workflows.py
+++ polling/periodic_sequence/workflows.py
@@ -6,8 +6,7 @@ from temporalio.common import RetryPolicy
 from temporalio.exceptions import ActivityError
 
 with workflow.unsafe.imports_passed_through():
-    from activities import compose_greeting
-    from obj import ComposeGreetingInput
+    from activities import compose_greeting, ComposeGreetingInput
 
 
 @workflow.defn
@@ -24,29 +23,24 @@ class GreetingWorkflow:
 class ChildWorkflow:
     @workflow.run
     async def run(self, name: str) -> str:
-        polling_interval = 5  # seconds
-        max_poll_attempts = 10
-        activities = workflow.execute_activity(
-            compose_greeting,
-            ComposeGreetingInput("Hello", name),
-            start_to_close_timeout=timedelta(seconds=4),
-            retry_policy=RetryPolicy(
-                initial_interval=timedelta(seconds=polling_interval),
-                maximum_attempts=max_poll_attempts,
-            ),
-        )
-
-        for i in range(max_poll_attempts):
+        attempt = 0
+        for i in range(10):
             try:
-                result = await activities
-                return result
+                polling_activities = await workflow.execute_activity(
+                    compose_greeting,
+                    ComposeGreetingInput("Hello", name),
+                    start_to_close_timeout=timedelta(seconds=4),
+                    retry_policy=RetryPolicy(
+                        maximum_attempts=1,
+                    ),
+                )
+                attempt += 1
+                return polling_activities
+
             except ActivityError:
-                print(f"Activity failed, retrying in {polling_interval} seconds")
+                workflow.logger.error("Activity failed, retrying in 5 seconds")
+            await asyncio.sleep(5)
 
-            await asyncio.sleep(polling_interval)
-
-            if i % 3 == 0:  # every 3rd iteration
-                workflow.continue_as_new(ChildWorkflow)
-
+            workflow.continue_as_new(name)
         # If we've reached here, it means all attempts failed
         raise Exception("Polling failed after all attempts")

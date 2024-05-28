03e41fec2b5a4f9f4bf4c34ae1340ea4aca05286
yashvanzara <yashvanzara@gmail.com>
2024-05-28 20:07:36 +0100

    fix: test name and await for result

--- tests/hello/hello_update_test.py
+++ tests/hello/hello_update_test.py
@@ -6,7 +6,7 @@ from temporalio.worker import Worker
 from hello.hello_update import GreetingWorkflow
 
 
-async def test_signal_workflow(client: Client):
+async def test_update_workflow(client: Client):
     task_queue_name = str(uuid.uuid4())
     async with Worker(client, task_queue=task_queue_name, workflows=[GreetingWorkflow]):
         handle = await client.start_workflow(
@@ -19,4 +19,5 @@ async def test_signal_workflow(client: Client):
             GreetingWorkflow.update_workflow_status
         )
         assert "Workflow status updated" == update_result
+        assert "Hello, World!" == (await handle.result())
         assert WorkflowExecutionStatus.COMPLETED == (await handle.describe()).status

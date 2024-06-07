62df4cb2ff4bd066619223892884cef1cab3e67a
Mike Nichols <mike.nichols@temporal.io>
2024-06-07 16:26:01 -0400

    fix missing sleep to properly poll inside activity

--- polling/frequent/activities.py
+++ polling/frequent/activities.py
@@ -1,6 +1,9 @@
+import asyncio
+import time
 from dataclasses import dataclass
 
 from temporalio import activity
+from temporalio.exceptions import CancelledError
 
 from polling.test_service import TestService
 
@@ -17,6 +20,16 @@ async def compose_greeting(input: ComposeGreetingInput) -> str:
     while True:
         try:
             result = test_service.get_service_result(input)
+            print(f"exiting activity ${result}")
             return result
-        except Exception:
+        except Exception as e:
+            # swallow exception since service is down
+            print(e)
+
+        try:
             activity.heartbeat("Invoking activity")
+        except CancelledError as exception:
+            # activity was either cancelled or workflow was completed or worker shut down
+            raise exception
+
+        await asyncio.sleep(1)

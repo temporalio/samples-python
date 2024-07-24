35b476d083438b3933c77000843fe1ef32512ae4
Drew Hoskins <166441821+drewhoskins-temporal@users.noreply.github.com>
2024-07-23 18:17:43 -0700

    Workflow Update and Signal handlers concurrency sample (#123)
    * Atomic message handlers sample
    
    * Remove resize jobs to reduce code size
    
    * Misc polish
    
    * Add test
    
    * Format code
    
    * Continue as new
    
    * Formatting
    
    * Feedback, readme, restructure files and directories
    
    * Format
    
    * More feedback.  Add test-continue-as-new flag.
    
    * Feedback; throw ApplicationFailures from update handlers
    
    * Formatting
    
    * __init__.py
    
    * Fix lint issues
    
    * Dan Feedback
    
    * More typehints
    
    * s/atomic/safe/
    
    * Fix and demo idempotency
    
    * Compatibility with 3.8
    
    * More feedback
    
    * Re-add tests
    
    * Fix flaky test
    
    * Improve update and tests
    
    * list -> set to fix a test
    * Return a struct rather than a raw value from the list for better hygiene
    * Remove test dependency on race conditions between health check and adding nodes.
    
    * Ruff linting
    
    * Use consistent verbs, improve health check
    
    * poe format
    
    * Minor sample improvements
    
    * Skip update tests under Java test server
    
    ---------
    
    Co-authored-by: Drew Hoskins <moograms@gmail.com>
    Co-authored-by: Dan Davison <dan.davison@temporal.io>

---
+++ updates_and_signals/safe_message_handlers/starter.py
@@ -0,0 +1,84 @@
+import argparse
+import asyncio
+import logging
+import uuid
+from typing import Optional
+
+from temporalio import common
+from temporalio.client import Client, WorkflowHandle
+
+from updates_and_signals.safe_message_handlers.workflow import (
+    ClusterManagerAssignNodesToJobInput,
+    ClusterManagerDeleteJobInput,
+    ClusterManagerInput,
+    ClusterManagerWorkflow,
+)
+
+
+async def do_cluster_lifecycle(wf: WorkflowHandle, delay_seconds: Optional[int] = None):
+
+    await wf.signal(ClusterManagerWorkflow.start_cluster)
+
+    print("Assigning jobs to nodes...")
+    allocation_updates = []
+    for i in range(6):
+        allocation_updates.append(
+            wf.execute_update(
+                ClusterManagerWorkflow.assign_nodes_to_job,
+                ClusterManagerAssignNodesToJobInput(
+                    total_num_nodes=2, job_name=f"task-{i}"
+                ),
+            )
+        )
+    await asyncio.gather(*allocation_updates)
+
+    print(f"Sleeping for {delay_seconds} second(s)")
+    if delay_seconds:
+        await asyncio.sleep(delay_seconds)
+
+    print("Deleting jobs...")
+    deletion_updates = []
+    for i in range(6):
+        deletion_updates.append(
+            wf.execute_update(
+                ClusterManagerWorkflow.delete_job,
+                ClusterManagerDeleteJobInput(job_name=f"task-{i}"),
+            )
+        )
+    await asyncio.gather(*deletion_updates)
+
+    await wf.signal(ClusterManagerWorkflow.shutdown_cluster)
+
+
+async def main(should_test_continue_as_new: bool):
+    # Connect to Temporal
+    client = await Client.connect("localhost:7233")
+
+    print("Starting cluster")
+    cluster_manager_handle = await client.start_workflow(
+        ClusterManagerWorkflow.run,
+        ClusterManagerInput(test_continue_as_new=should_test_continue_as_new),
+        id=f"ClusterManagerWorkflow-{uuid.uuid4()}",
+        task_queue="safe-message-handlers-task-queue",
+        id_reuse_policy=common.WorkflowIDReusePolicy.TERMINATE_IF_RUNNING,
+    )
+    delay_seconds = 10 if should_test_continue_as_new else 1
+    await do_cluster_lifecycle(cluster_manager_handle, delay_seconds=delay_seconds)
+    result = await cluster_manager_handle.result()
+    print(
+        f"Cluster shut down successfully."
+        f" It had {result.num_currently_assigned_nodes} nodes assigned at the end."
+    )
+
+
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.INFO)
+    parser = argparse.ArgumentParser(description="Atomic message handlers")
+    parser.add_argument(
+        "--test-continue-as-new",
+        help="Make the ClusterManagerWorkflow continue as new before shutting down",
+        action="store_true",
+        default=False,
+    )
+    args = parser.parse_args()
+    asyncio.run(main(args.test_continue_as_new))

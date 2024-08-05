5c76f739b3055b6e16650b55799c7956653e7b14
Dan Davison <dan.davison@temporal.io>
2024-08-05 13:36:23 -0400

    Experimenting with cancellation

--- dan/cancellation_and_handlers_etc.py
+++ dan/cancellation_and_handlers_etc.py
@@ -1,16 +1,23 @@
 import asyncio
-from typing import NoReturn
+from datetime import timedelta
 
 import rich
 import temporalio
 import temporalio.client
 import temporalio.exceptions
-from temporalio import common, workflow
+from temporalio import activity, common, workflow
 from temporalio.client import Client, WorkflowUpdateStage
+from temporalio.common import RetryPolicy
 from temporalio.worker import Worker
 
 wid = __file__
 tq = "tq"
+
+
+@activity.defn
+async def my_activity():
+    print("in activity")
+    raise Exception("activity error")
 
 
 @workflow.defn
@@ -19,29 +26,37 @@ class Workflow:
     A workflow that is canceled while handlers are running.
     """
 
+    def __init__(self) -> None:
+        self.handler_in_progress = False
+
     @workflow.run
-    async def run(self) -> NoReturn:
+    async def run(self) -> str:
+        await workflow.wait_condition(lambda: self.handler_in_progress)
         try:
-            await workflow.wait_condition(lambda: False)
-        except asyncio.CancelledError as err:
-            print(
-                f"[red]Workflow: asyncio.CancelledError while waiting forever: __{err.__class__.__name__}__[/red]"
+            await workflow.execute_activity(
+                my_activity,
+                start_to_close_timeout=timedelta(seconds=10),
+                retry_policy=RetryPolicy(maximum_attempts=1),
             )
-            # Will wait forever, since update handler currently sees no cancellation
-            await workflow.wait_condition(workflow.all_handlers_finished)
+        except temporalio.exceptions.ActivityError as err:
+            print(f"[red]Workflow: ActivityError: {err.__class__.__name__}[/red]")
+        return "wf-result"
 
     @workflow.update
-    async def my_update(self) -> NoReturn:
-        await workflow.wait_condition(lambda: False)
-
-    @workflow.signal
-    async def my_signal(self) -> NoReturn:
-        await workflow.wait_condition(lambda: False)
+    async def my_update(self) -> str:
+        self.handler_in_progress = True
+        # await workflow.wait_condition(lambda: False)
+        return "update-result"
 
 
 async def main():
     client = await Client.connect("localhost:7233")
-    async with Worker(client, task_queue=tq, workflows=[Workflow]):
+    async with Worker(
+        client,
+        task_queue=tq,
+        workflows=[Workflow],
+        activities=[my_activity],
+    ):
         wf_handle = await client.start_workflow(
             Workflow.run,
             id=wid,
@@ -54,9 +69,7 @@ async def main():
             Workflow.my_update, wait_for_stage=WorkflowUpdateStage.ACCEPTED
         )
 
-        await wf_handle.signal(Workflow.my_signal)
-
-        await wf_handle.cancel()
+        # await wf_handle.cancel()
 
         try:
             print(await upd_handle.result())

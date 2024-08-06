75a36081116997fc877685de84c3bce12a35de77
Dan Davison <dan.davison@temporal.io>
2024-08-06 18:48:18 -0400

    Update example for python docs

--- dan/docs_message_passing.py
+++ dan/docs_message_passing.py
@@ -1,13 +1,9 @@
 import asyncio
+from dataclasses import dataclass
 from enum import IntEnum
 from typing import Optional
 
-from temporalio import common, workflow
-from temporalio.client import Client
-from temporalio.worker import Worker
-
-wid = __file__
-tq = "tq"
+from temporalio import workflow
 
 
 class Language(IntEnum):
@@ -18,61 +14,100 @@ class Language(IntEnum):
     Portuguese = 5
 
 
-GREETINGS = {
-    Language.English: "Hello, world!",
-    Language.Chinese: "你好，世界!",
-}
+@dataclass
+class GetLanguagesInput:
+    supported_only: bool
+
+
+@dataclass
+class ApproveInput:
+    name: str
 
 
 @workflow.defn
 class GreetingWorkflow:
     def __init__(self) -> None:
         self.approved_for_release = False
+        self.approver_name: Optional[str] = None
         self.language = Language.English
+        self.greetings = {
+            Language.English: "Hello, world!",
+            Language.Chinese: "你好，世界!",
+        }
 
     @workflow.run
-    async def run(self) -> Optional[str]:
+    async def run(self) -> str:
         await workflow.wait_condition(lambda: self.approved_for_release)
-        return GREETINGS[self.language]
+        return self.greetings[self.language]
 
     @workflow.query
-    def get_language(self) -> Language:
-        return self.language
+    def get_languages(self, input: GetLanguagesInput) -> list[Language]:
+        # A Query handler returns a value but must not mutate the Workflow state.
+        if input.supported_only:
+            return [lang for lang in Language if lang in self.greetings]
+        else:
+            return list(Language)
 
     @workflow.signal
-    def approve(self) -> None:
+    def approve(self, input: ApproveInput) -> None:
+        # A Signal handler mutates the Workflow state but cannot return a value.
         self.approved_for_release = True
+        self.approver_name = input.name
 
     @workflow.update
     def set_language(self, language: Language) -> Language:
+        # An Update handler can mutate the Workflow state and return a value.
         previous_language, self.language = self.language, language
         return previous_language
 
     @set_language.validator
     def validate_language(self, language: Language) -> None:
-        if language not in GREETINGS:
+        if language not in self.greetings:
+            # In an Update validator you raise any exception to reject the Update.
             raise ValueError(f"{language.name} is not supported")
+
+    @workflow.query
+    def get_language(self) -> Language:
+        return self.language
+
+
+from temporalio.client import Client, WorkflowUpdateStage
+from temporalio.worker import Worker
 
 
 async def main():
     client = await Client.connect("localhost:7233")
     async with Worker(
         client,
-        task_queue=tq,
+        task_queue="my-task-queue",
         workflows=[GreetingWorkflow],
     ):
         wf_handle = await client.start_workflow(
             GreetingWorkflow.run,
-            id=wid,
-            task_queue=tq,
-            id_reuse_policy=common.WorkflowIDReusePolicy.TERMINATE_IF_RUNNING,
+            id="greeting-workflow-1234",
+            task_queue="my-task-queue",
         )
-        await wf_handle.execute_update(GreetingWorkflow.set_language, Language.Chinese)
-        if False:
-            await wf_handle.execute_update(
-                GreetingWorkflow.set_language, Language.French
-            )
-        await wf_handle.signal(GreetingWorkflow.approve)
+        supported_languages = await wf_handle.query(
+            GreetingWorkflow.get_languages, GetLanguagesInput(supported_only=True)
+        )
+        print(f"supported languages: {supported_languages}")
+
+        previous_language = await wf_handle.execute_update(
+            GreetingWorkflow.set_language, Language.Chinese
+        )
+        current_language = await wf_handle.query(GreetingWorkflow.get_language)
+        print(f"language changed: {previous_language.name} -> {current_language.name}")
+
+        update_handle = await wf_handle.start_update(
+            GreetingWorkflow.set_language,
+            Language.English,
+            wait_for_stage=WorkflowUpdateStage.ACCEPTED,
+        )
+        previous_language = await update_handle.result()
+        current_language = await wf_handle.query(GreetingWorkflow.get_language)
+        print(f"language changed: {previous_language.name} -> {current_language.name}")
+
+        await wf_handle.signal(GreetingWorkflow.approve, ApproveInput(name=""))
         print(await wf_handle.result())
 
 

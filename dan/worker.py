d81dce9f19742dfa7d87530b264b7617d6d3ca11
Dan Davison <dan.davison@temporal.io>
2024-08-27 10:26:34 -0400

    guess workflow based on file modification time

--- dan/worker.py
+++ dan/worker.py
@@ -1,12 +1,31 @@
 import asyncio
+import os
+from importlib import import_module
+from pathlib import Path
 
 from temporalio.client import Client
 from temporalio.worker import Worker
 
 from dan.constants import NAMESPACE, TASK_QUEUE
 
-# Don't commit the line importing the Workflow class
-from dan.two_updates import Workflow
+
+def get_last_edited_workflow_module():
+    dan_dir = Path(__file__).parent
+    workflow_files = []
+    for f in dan_dir.glob("*.py"):
+        if f.is_file():
+            try:
+                module = import_module(f"dan.{f.stem}")
+            except NameError:
+                continue
+            if hasattr(module, "Workflow"):
+                workflow_files.append(f)
+    latest_file = max(workflow_files, key=os.path.getmtime)
+    return import_module(f"dan.{latest_file.stem}")
+
+
+latest_module = get_last_edited_workflow_module()
+Workflow = latest_module.Workflow
 
 interrupt_event = asyncio.Event()
 

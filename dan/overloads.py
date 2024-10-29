a15994d763cea26ccedcaaa3794bd48af07ac910
Dan Davison <dan.davison@temporal.io>
2024-10-29 03:17:51 -0400

    overloads

---
+++ dan/overloads.py
@@ -0,0 +1,34 @@
+import asyncio
+from typing import cast
+
+from opentelemetry.sdk.trace import Tracer
+from temporalio import common, workflow
+
+from dan.utils import connect
+from dan.utils.otel import create_tracer_provider
+
+provider = create_tracer_provider("Workflow")
+tracer = cast(Tracer, provider.get_tracer(__name__))
+
+
+@workflow.defn
+class Workflow:
+    @workflow.run
+    async def run(self, a: int) -> int:
+        return a + 1
+
+
+async def main():
+    client = await connect()
+    h = await client.start_workflow(
+        "Workflow",
+        args=[7],
+        id="wid",
+        task_queue="tq",
+        id_conflict_policy=common.WorkflowIDConflictPolicy.TERMINATE_EXISTING,
+    )
+    print(await h.result())
+
+
+if __name__ == "__main__":
+    asyncio.run(main())

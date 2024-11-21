b054fd8d743c014bb457d4fa6ce771c893a4e615
Drew Hoskins <166441821+drewhoskins-temporal@users.noreply.github.com>
2024-11-20 17:22:48 -0800

    Apply suggestions from code review
    Co-authored-by: Dan Davison <dan.davison@temporal.io>

--- polling/test_service.py
+++ polling/test_service.py
@@ -3,7 +3,7 @@ class TestService:
         self.error_attempts = 5
 
     async def get_service_result(self, input, attempt: int):
-        print(f"Attempt {attempt}" f" of {self.error_attempts} to invoke service")
-        if attempt % self.error_attempts == 0:
+        print(f"Attempt {attempt} of {self.error_attempts} to invoke service")
+        if attempt > self.error_attempts:
             return f"{input.greeting}, {input.name}!"
         raise Exception("service is down")

fb2f927e392a704e9ada34f2e1ca70aaee86b6b3
Drew Hoskins <drew.hoskins@temporal.io>
2024-11-20 16:50:02 -0800

    Fix Infrequent Polling sample

--- polling/infrequent/activities.py
+++ polling/infrequent/activities.py
@@ -16,4 +16,4 @@ async def compose_greeting(input: ComposeGreetingInput) -> str:
     test_service = TestService()
     # If this raises an exception because it's not done yet, the activity will
     # continually be scheduled for retry
-    return await test_service.get_service_result(input)
+    return await test_service.get_service_result(input, activity.info().attempt)
--- polling/test_service.py
+++ polling/test_service.py
@@ -1,14 +1,12 @@
 class TestService:
     def __init__(self):
-        self.try_attempts = 0
         self.error_attempts = 5
 
-    async def get_service_result(self, input):
+    async def get_service_result(self, input, attempt: int):
         print(
-            f"Attempt {self.try_attempts}"
+            f"Attempt {attempt}"
             f" of {self.error_attempts} to invoke service"
         )
-        self.try_attempts += 1
-        if self.try_attempts % self.error_attempts == 0:
+        if attempt % self.error_attempts == 0:
             return f"{input.greeting}, {input.name}!"
         raise Exception("service is down")

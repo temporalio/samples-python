4e70d98844771125f977f6c331da0f03fe6732d2
Dan Davison <dan.davison@temporal.io>
2025-01-21 20:47:05 -0500

    updates do not block CAN

---
+++ dan/updates_do_not_block_continue_as_new.py
@@ -0,0 +1,77 @@
+"""
+In this test the client code sends an update, guaranteeing that the workflow worker is
+processing a workflow task (WFT) at the time that the update is admitted by the server. To
+do this it must synchronize the workflow and client. This uses techniques that should
+never be used in real workflows. The synchronization must be threading-based as opposed to
+asyncio-based, since the point is to not allow the workflow to yield while it is waiting
+for notification from the client. In order for the workflow and client to share the same
+module namespace, we use UnsandboxedWorkflowRunner. But this means that the workflow and
+client code execute in the same thread. Therefore we create a new thread for the client
+code to execute in, so that the two can use thread-blocking waits on the shared
+threading.Event object.
+"""
+
+import asyncio
+import threading
+
+from temporalio import workflow
+
+from dan.utils import connect
+from dan.utils.client import admitted_update_task, start_workflow
+
+# See docstring at top of file.
+first_run_wft_is_in_progress = threading.Event()
+update_has_been_admitted = threading.Event()
+
+
+@workflow.defn
+class Workflow:
+    def __init__(self):
+        self.received_update = False
+
+    @workflow.run
+    async def run(self) -> str:
+        """
+        Continue as new once, then return the current run ID.
+        """
+
+        info = workflow.info()
+        if info.continued_run_id is not None:
+            # The update is probably delivered in the first post-CAN WFT, in which case
+            # the following wait_condition is not needed. However, correct behavior does
+            # not require this to be true.
+            await workflow.wait_condition(lambda: self.received_update)
+            return info.run_id
+
+        workflow.continue_as_new()
+
+    @workflow.update
+    async def update(self) -> str:
+        """Update handler that returns the current run ID"""
+        self.received_update = True
+        return workflow.info().run_id
+
+
+async def main():
+    client = await connect()
+    handle = await start_workflow(Workflow.run, client=client)
+
+    # See docstring at top of file.
+    # Cause an update to be admitted while the first WFT is in progress
+    update_task = await admitted_update_task(
+        client, handle, Workflow.update, "update-id"
+    )
+    # Unblock the workflow so that it responds to the WFT with a CAN command.
+    update_has_been_admitted.set()
+    # The workflow will now CAN. Wait for the update result
+    update_run_id = await update_task
+    # The update should have been handled on the post-CAN run.
+    assert (
+        handle.first_execution_run_id
+        and update_run_id
+        and update_run_id != handle.first_execution_run_id
+    ), "Expected update to be handled on post-CAN run"
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
--- dan/utils/client.py
+++ dan/utils/client.py
@@ -1,6 +1,8 @@
+import asyncio
 import os
+import time
 from datetime import timedelta
-from typing import Optional, TypeVar
+from typing import Awaitable, Callable, Optional, TypeVar
 
 from opentelemetry import trace
 from temporalio import common
@@ -10,13 +12,15 @@ from temporalio.api.workflowservice.v1 import PollWorkflowExecutionUpdateRequest
 from temporalio.client import Client, WorkflowHandle
 from temporalio.service import RPCError, RPCStatusCode
 from temporalio.types import MethodAsyncNoParam
+from temporalio.workflow import UpdateMethodMultiParam
 
 from dan.constants import TASK_QUEUE, WORKFLOW_ID
 from dan.utils import connect
 from dan.utils.otel import create_tracer_provider
 
-S = TypeVar("S")
 R = TypeVar("R")
+S = TypeVar("S")
+T = TypeVar("T")
 
 
 async def start_workflow(
@@ -41,6 +45,30 @@ async def start_workflow(
     )
 
 
+# The update utilities below are copied from
+# https://github.com/temporalio/sdk-python/blob/main/tests/helpers/__init__.py
+
+
+async def admitted_update_task(
+    client: Client,
+    handle: WorkflowHandle,
+    update_method: UpdateMethodMultiParam,
+    id: str,
+    **kwargs,
+) -> asyncio.Task:
+    """
+    Return an asyncio.Task for an update after waiting for it to be admitted.
+    """
+    update_task = asyncio.create_task(
+        handle.execute_update(update_method, id=id, **kwargs)
+    )
+    await assert_eq_eventually(
+        True,
+        lambda: update_has_been_admitted(client, handle.id, id),
+    )
+    return update_task
+
+
 async def update_has_been_admitted(
     client: Client, workflow_id: str, update_id: str
 ) -> bool:
@@ -59,3 +87,22 @@ async def update_has_been_admitted(
         if err.status != RPCStatusCode.NOT_FOUND:
             raise
         return False
+
+
+async def assert_eq_eventually(
+    expected: T,
+    fn: Callable[[], Awaitable[T]],
+    *,
+    timeout: timedelta = timedelta(seconds=10),
+    interval: timedelta = timedelta(milliseconds=200),
+) -> None:
+    start_sec = time.monotonic()
+    last_value = None
+    while timedelta(seconds=time.monotonic() - start_sec) < timeout:
+        last_value = await fn()
+        if expected == last_value:
+            return
+        await asyncio.sleep(interval.total_seconds())
+    assert (
+        expected == last_value
+    ), f"timed out waiting for equal, asserted against last value of {last_value}"

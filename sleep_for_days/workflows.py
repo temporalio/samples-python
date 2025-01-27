ab65e33e7ee0eb73daba01e82ecdb51e6a67c941
Thomas Hardy <thomas.hardy@temporal.io>
2025-01-27 10:24:07 -0800

    added race between signal and sleep to terminate immediately on signal

--- sleep_for_days/workflows.py
+++ sleep_for_days/workflows.py
@@ -18,7 +18,10 @@ class SleepForDaysWorkflow:
     async def run(self, input: SleepForDaysInput) -> str:
         while(not self.is_complete):
             await workflow.execute_activity(send_email, SendEmailInput(f"{input.numOfDays} until the next email"), start_to_close_timeout=timedelta(seconds=10))
-            await asyncio.sleep(timedelta(days=input.numOfDays).seconds)
+            await workflow.wait([
+                asyncio.create_task(workflow.sleep(input.numOfDays * 24 * 60 * 60)),
+                asyncio.create_task(workflow.wait_condition(lambda: self.is_complete))
+            ], return_when=asyncio.FIRST_COMPLETED)
         return "done!"
     
     @workflow.signal

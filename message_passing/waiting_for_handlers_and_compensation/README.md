d9658d51fac8d1d8647c3fc9f0457146aaeae7b7
Dan Davison <dan.davison@temporal.io>
2025-01-28 10:05:20 -0500

    Waiting for handlers to finish in all exit cases + abort and compensation in a message handler (#144)

---
+++ message_passing/waiting_for_handlers_and_compensation/README.md
@@ -0,0 +1,39 @@
+# Waiting for message handlers, and performing compensation and cleanup in message handlers
+
+This sample demonstrates how to do the following:
+
+1. Ensure that all update/signal handlers are finished before a successful
+    workflow return, and on workflow cancellation and failure.
+2. Perform compensation/cleanup in an update handler when the workflow is
+    cancelled or fails.
+
+For a simpler sample showing how to do (1) without (2), see [safe_message_handlers](../safe_message_handlers/README.md).
+
+To run, open two terminals and `cd` to this directory in them.
+
+Run the worker in one terminal:
+
+    poetry run python worker.py
+
+And run the workflow-starter code in the other terminal:
+
+    poetry run python starter.py
+
+
+Here's the output you'll see:
+
+```
+workflow exit type: SUCCESS
+    🟢 caller received update result
+    🟢 caller received workflow result
+
+
+workflow exit type: FAILURE
+    🔴 caught exception while waiting for update result: Workflow update failed: The update failed because the workflow run exited
+    🔴 caught exception while waiting for workflow result: Workflow execution failed: deliberately failing workflow
+
+
+workflow exit type: CANCELLATION
+    🔴 caught exception while waiting for update result: Workflow update failed: The update failed because the workflow run exited
+    🔴 caught exception while waiting for workflow result: Workflow execution failed: Workflow cancelled
+```
---
+++ message_passing/waiting_for_handlers_and_compensation/worker.py
@@ -0,0 +1,46 @@
+import asyncio
+import logging
+
+from temporalio.client import Client
+from temporalio.worker import Worker
+
+from message_passing.waiting_for_handlers_and_compensation import TASK_QUEUE
+from message_passing.waiting_for_handlers_and_compensation.activities import (
+    activity_executed_by_update_handler,
+    activity_executed_by_update_handler_to_perform_compensation,
+    activity_executed_to_perform_workflow_compensation,
+)
+from message_passing.waiting_for_handlers_and_compensation.workflows import (
+    WaitingForHandlersAndCompensationWorkflow,
+)
+
+interrupt_event = asyncio.Event()
+
+
+async def main():
+    logging.basicConfig(level=logging.INFO)
+
+    client = await Client.connect("localhost:7233")
+
+    async with Worker(
+        client,
+        task_queue=TASK_QUEUE,
+        workflows=[WaitingForHandlersAndCompensationWorkflow],
+        activities=[
+            activity_executed_by_update_handler,
+            activity_executed_by_update_handler_to_perform_compensation,
+            activity_executed_to_perform_workflow_compensation,
+        ],
+    ):
+        logging.info("Worker started, ctrl+c to exit")
+        await interrupt_event.wait()
+        logging.info("Shutting down")
+
+
+if __name__ == "__main__":
+    loop = asyncio.new_event_loop()
+    try:
+        loop.run_until_complete(main())
+    except KeyboardInterrupt:
+        interrupt_event.set()
+        loop.run_until_complete(loop.shutdown_asyncgens())

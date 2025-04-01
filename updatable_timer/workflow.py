88e3d6e60ba7dce85573a4f8f3697290177f18dc
Maxim Fateev <mfateev@gmail.com>
2025-04-01 10:24:48 -0700

    Fixes per PR review feedback. Refactored workflow to use workflow.init. Added unit test.

--- updatable_timer/workflow.py
+++ updatable_timer/workflow.py
@@ -8,28 +8,25 @@ from updatable_timer.updatable_timer_lib import UpdatableTimer
 
 @workflow.defn
 class Workflow:
-    def __init__(self):
-        self.timer: Optional[UpdatableTimer] = None
-
-    @workflow.run
-    async def run(self, wake_up_time: float):
+    @workflow.init
+    def __init__(self, wake_up_time: float) -> None:
         self.timer = UpdatableTimer(
             datetime.fromtimestamp(wake_up_time, tz=timezone.utc)
         )
+
+    @workflow.run
+    async def run(self, wake_up_time: float):
         await self.timer.sleep()
 
     @workflow.signal
-    async def update_wake_up_time(self, wake_up_time: float):
-        # Deals with situation when the signal method is called before the run method.
-        # This happens when a workflow task is executed after a signal is received
-        # or when a workflow is started using the signal-with-start.
-        await workflow.wait_condition(lambda: self.timer is not None)
-        assert self.timer is not None  # for mypy
+    async def update_wake_up_time(self, wake_up_time: float) -> None:
+        workflow.logger.info(f"update_wake_up_time: {wake_up_time}")
+
         self.timer.update_wake_up_time(
             datetime.fromtimestamp(wake_up_time, tz=timezone.utc)
         )
 
     @workflow.query
-    def get_wake_up_time(self):
-        assert self.timer is not None  # for mypy
-        return self.timer.get_wake_up_time()
+    def get_wake_up_time(self) -> float:
+        workflow.logger.info(f"get_wake_up_time")
+        return float(self.timer.get_wake_up_time().timestamp())

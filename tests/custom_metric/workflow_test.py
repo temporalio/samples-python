84fdfeef7f2960872164714bfcee21dd990fb003
GSmithApps <grant.smith@temporal.io>
2025-04-21 09:52:18 -0500

    responding to code review

--- tests/custom_metric/workflow_test.py
+++ tests/custom_metric/workflow_test.py
@@ -12,21 +12,21 @@ activity_counter = 0
 
 
 async def test_custom_metric_workflow(client: Client):
-    @activity.defn(name="print_message")
+    @activity.defn(name="print_and_sleep")
     async def print_message_mock():
         global activity_counter
         activity_counter += 1
 
-        async with Worker(
-            client,
+    async with Worker(
+        client,
+        task_queue=_TASK_QUEUE,
+        workflows=[StartTwoActivitiesWorkflow],
+        activities=[print_message_mock],
+    ):
+        result = await client.execute_workflow(
+            StartTwoActivitiesWorkflow.run,
+            id=str(uuid.uuid4()),
             task_queue=_TASK_QUEUE,
-            workflows=[StartTwoActivitiesWorkflow],
-            activities=[print_message_mock],
-        ):
-            result = await client.execute_workflow(
-                StartTwoActivitiesWorkflow.run,
-                id=str(uuid.uuid4()),
-                task_queue=_TASK_QUEUE,
-            )
-            assert result is None
-            assert activity_counter == 2
+        )
+        assert result is None
+        assert activity_counter == 2

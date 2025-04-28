1afa91ebc1eac8b55062b76f3c12609372a8f834
Dan Davison <dan.davison@temporal.io>
2025-04-28 18:02:44 -0400

    Respond to upstream

--- hello_nexus/simple.py
+++ hello_nexus/simple.py
@@ -5,19 +5,20 @@ from temporalio import workflow
 from temporalio.workflow import NexusClient
 
 
-# TODO(dan): make this decorator usable without parens
-@nexusrpc.handler.service()
+@nexusrpc.handler.service
 class MyNexusService:
     @nexusrpc.handler.sync_operation
     async def my_op(self, input: str, _: nexusrpc.handler.StartOperationOptions) -> str:
         return f"{input}-result"
 
 
-@workflow.run
-async def run(self, message: str) -> str:
-    nexus_client = NexusClient(
-        MyNexusService,
-        "my-endpoint",
-        schedule_to_close_timeout=timedelta(seconds=30),
-    )
-    return await nexus_client.execute_operation(MyNexusService.my_op, message)
+@workflow.defn
+class Workflow:
+    @workflow.run
+    async def run(self, message: str) -> str:
+        nexus_client = NexusClient(
+            MyNexusService,
+            "my-endpoint",
+            schedule_to_close_timeout=timedelta(seconds=30),
+        )
+        return await nexus_client.execute_operation(MyNexusService.my_op, message)

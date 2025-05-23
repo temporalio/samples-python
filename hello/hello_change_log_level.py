598c1af2f2d8c3b092d024f717c5baa4008d1bdd
GSmithApps <grant.smith@temporal.io>
2025-05-23 12:44:34 -0500

    added sample showing how to change log levels

---
+++ hello/hello_change_log_level.py
@@ -0,0 +1,58 @@
+"""
+Changes the log level of workflow task failures from WARN to ERROR.
+"""
+
+import asyncio, logging, sys
+from temporalio import workflow
+from temporalio.client import Client
+from temporalio.worker import Worker
+
+# --- Begin logging set‑up ----------------------------------------------------------
+logging.basicConfig(
+    stream=sys.stdout,
+    level=logging.INFO,
+    format="%(asctime)s  %(levelname)-8s  %(name)s  %(message)s",
+)
+
+
+class CustomLogFilter(logging.Filter):
+    def filter(self, record):
+        msg = record.getMessage()
+        if (
+            msg.startswith("Failed activation on workflow")
+            and " with ID " in msg
+            and " and run ID " in msg
+            and record.levelno < logging.ERROR
+        ):
+            record.levelno = logging.ERROR
+            record.levelname = logging.getLevelName(logging.ERROR)
+        return True
+
+
+logging.getLogger("temporalio.worker._workflow_instance").addFilter(CustomLogFilter())
+# --- End logging set‑up ----------------------------------------------------------
+
+
+@workflow.defn
+class GreetingWorkflow:
+    @workflow.run
+    async def run(self):
+        raise RuntimeError("This is a test error")
+
+
+async def main():
+    client = await Client.connect("localhost:7233")
+    async with Worker(
+        client,
+        task_queue="hello-task-queue",
+        workflows=[GreetingWorkflow],
+    ):
+        await client.execute_workflow(
+            GreetingWorkflow.run,
+            id="hello-workflow-id",
+            task_queue="hello-task-queue",
+        )
+
+
+if __name__ == "__main__":
+    asyncio.run(main())

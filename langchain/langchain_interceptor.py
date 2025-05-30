89c2f4d05c9018082ce320e5b539651f9f1f8960
Maxim Fateev <mfateev@gmail.com>
2025-05-30 08:20:09 -0700

    Fixed imports

--- langchain/langchain_interceptor.py
+++ langchain/langchain_interceptor.py
@@ -1,32 +1,23 @@
 from __future__ import annotations
-import asyncio
-from contextvars import copy_context
 
-from langsmith import get_tracing_context
-from langsmith.run_helpers import _set_tracing_context
-from temporalio import workflow
+from temporalio import workflow, worker, converter, client, api, activity
 
 with workflow.unsafe.imports_passed_through():
     from contextlib import contextmanager
     from typing import Any, Mapping, Protocol, Type
 
-    import temporalio.activity
-    import temporalio.api.common.v1
-    import temporalio.client
-    import temporalio.converter
-    import temporalio.worker
-    import temporalio.workflow
-    from langsmith import Client, trace, tracing_context
+
+    from langsmith import trace, tracing_context
     from langsmith.run_helpers import get_current_run_tree
 
 # Header key for LangChain context
 LANGCHAIN_CONTEXT_KEY = "langchain-context"
 
 class _InputWithHeaders(Protocol):
-    headers: Mapping[str, temporalio.api.common.v1.Payload]
+    headers: Mapping[str, api.common.v1.Payload]
 
 def set_header_from_context(
-    input: _InputWithHeaders, payload_converter: temporalio.converter.PayloadConverter
+    input: _InputWithHeaders, payload_converter: converter.PayloadConverter
 ) -> None:
     # Get current LangChain run tree
     run_tree = get_current_run_tree()
@@ -39,7 +30,7 @@ def set_header_from_context(
 
 @contextmanager
 def context_from_header(
-    input: _InputWithHeaders, payload_converter: temporalio.converter.PayloadConverter
+    input: _InputWithHeaders, payload_converter: converter.PayloadConverter
 ):
     payload = input.headers.get(LANGCHAIN_CONTEXT_KEY)
     if payload:
@@ -51,124 +42,124 @@ def context_from_header(
         yield
 
 class LangChainContextPropagationInterceptor(
-    temporalio.client.Interceptor, temporalio.worker.Interceptor
+    client.Interceptor, worker.Interceptor
 ):
     """Interceptor that propagates LangChain context through Temporal."""
 
     def __init__(
         self,
-        payload_converter: temporalio.converter.PayloadConverter = temporalio.converter.default().payload_converter,
+        payload_converter: converter.PayloadConverter = converter.default().payload_converter,
     ) -> None:
         self._payload_converter = payload_converter
 
     def intercept_client(
-        self, next: temporalio.client.OutboundInterceptor
-    ) -> temporalio.client.OutboundInterceptor:
+        self, next: client.OutboundInterceptor
+    ) -> client.OutboundInterceptor:
         return _LangChainContextPropagationClientOutboundInterceptor(
             next, self._payload_converter
         )
 
     def intercept_activity(
-        self, next: temporalio.worker.ActivityInboundInterceptor
-    ) -> temporalio.worker.ActivityInboundInterceptor:
+        self, next: worker.ActivityInboundInterceptor
+    ) -> worker.ActivityInboundInterceptor:
         return _LangChainContextPropagationActivityInboundInterceptor(next)
 
     def workflow_interceptor_class(
-        self, input: temporalio.worker.WorkflowInterceptorClassInput
+        self, input: worker.WorkflowInterceptorClassInput
     ) -> Type[_LangChainContextPropagationWorkflowInboundInterceptor]:
         return _LangChainContextPropagationWorkflowInboundInterceptor
 
 class _LangChainContextPropagationClientOutboundInterceptor(
-    temporalio.client.OutboundInterceptor
+    client.OutboundInterceptor
 ):
     def __init__(
         self,
-        next: temporalio.client.OutboundInterceptor,
-        payload_converter: temporalio.converter.PayloadConverter,
+        next: client.OutboundInterceptor,
+        payload_converter: converter.PayloadConverter,
     ) -> None:
         super().__init__(next)
         self._payload_converter = payload_converter
 
     async def start_workflow(
-        self, input: temporalio.client.StartWorkflowInput
-    ) -> temporalio.client.WorkflowHandle[Any, Any]:
+        self, input: client.StartWorkflowInput
+    ) -> client.WorkflowHandle[Any, Any]:
         with trace(name=f"start_workflow:{input.workflow}"):
             set_header_from_context(input, self._payload_converter)
             return await super().start_workflow(input)
 
 class _LangChainContextPropagationActivityInboundInterceptor(
-    temporalio.worker.ActivityInboundInterceptor
+    worker.ActivityInboundInterceptor
 ):
     async def execute_activity(
-        self, input: temporalio.worker.ExecuteActivityInput
+        self, input: worker.ExecuteActivityInput
     ) -> Any:
         if isinstance(input.fn, str):
             name = input.fn
         elif callable(input.fn):
-            defn = temporalio.activity._Definition.from_callable(input.fn)
+            defn = activity._Definition.from_callable(input.fn)
             if not defn or not defn.name:
                 name = "unknown"
             name = defn.name
 
-        with context_from_header(input, temporalio.activity.payload_converter()):
+        with context_from_header(input, activity.payload_converter()):
             with trace(name=f"execute_activity:{name}"):
                 return await self.next.execute_activity(input)
 
 class _LangChainContextPropagationWorkflowInboundInterceptor(
-    temporalio.worker.WorkflowInboundInterceptor
+    worker.WorkflowInboundInterceptor
 ):
-    def init(self, outbound: temporalio.worker.WorkflowOutboundInterceptor) -> None:
+    def init(self, outbound: worker.WorkflowOutboundInterceptor) -> None:
         self.next.init(_LangChainContextPropagationWorkflowOutboundInterceptor(outbound))
 
     async def execute_workflow(
-        self, input: temporalio.worker.ExecuteWorkflowInput
+        self, input: worker.ExecuteWorkflowInput
     ) -> Any:
         if isinstance(input.run_fn, str):
             name = input.run_fn
         elif callable(input.run_fn):
-            defn = temporalio.workflow._Definition.from_run_fn(input.run_fn)
+            defn = workflow._Definition.from_run_fn(input.run_fn)
             if defn is None or defn.name is None:
                 name = "unknown"
             name = defn.name
 
-        with context_from_header(input, temporalio.workflow.payload_converter()):
+        with context_from_header(input, workflow.payload_converter()):
             # This is a sandbox friendly way to write
             # with trace(...):
             #   return await self.next.execute_workflow(input)
-            with temporalio.workflow.unsafe.sandbox_unrestricted():
+            with workflow.unsafe.sandbox_unrestricted():
                 t = trace(name=f"execute_workflow:{name}", run_id=workflow.info().run_id)
             try:
                 return await self.next.execute_workflow(input)
             finally:
-                with temporalio.workflow.unsafe.sandbox_unrestricted():
+                with workflow.unsafe.sandbox_unrestricted():
                     # Cannot use __aexit__ because it's internally uses 
                     # loop.run_in_executor which is not available in the sandbox
                     t.__exit__()
 
 
 class _LangChainContextPropagationWorkflowOutboundInterceptor(
-    temporalio.worker.WorkflowOutboundInterceptor
+    worker.WorkflowOutboundInterceptor
 ):
     def start_activity(
-        self, input: temporalio.worker.StartActivityInput
-    ) -> temporalio.workflow.ActivityHandle:
-        with temporalio.workflow.unsafe.sandbox_unrestricted():
+        self, input: worker.StartActivityInput
+    ) -> workflow.ActivityHandle:
+        with workflow.unsafe.sandbox_unrestricted():
             t = trace(name=f"start_activity:{input.activity}", run_id=workflow.uuid4())
         try:
-            set_header_from_context(input, temporalio.workflow.payload_converter())
+            set_header_from_context(input, workflow.payload_converter())
             return self.next.start_activity(input)
         finally:
-            with temporalio.workflow.unsafe.sandbox_unrestricted():
+            with workflow.unsafe.sandbox_unrestricted():
                 t.__exit__()
 
     async def start_child_workflow(
-        self, input: temporalio.worker.StartChildWorkflowInput
-    ) -> temporalio.workflow.ChildWorkflowHandle:
-        with temporalio.workflow.unsafe.sandbox_unrestricted():
+        self, input: worker.StartChildWorkflowInput
+    ) -> workflow.ChildWorkflowHandle:
+        with workflow.unsafe.sandbox_unrestricted():
             t = trace(name=f"start_child_workflow:{input.workflow}", run_id=workflow.uuid4())
         try:
-            set_header_from_context(input, temporalio.workflow.payload_converter())
+            set_header_from_context(input, workflow.payload_converter())
             return await self.next.start_child_workflow(input)
         finally:
-            with temporalio.workflow.unsafe.sandbox_unrestricted():
+            with workflow.unsafe.sandbox_unrestricted():
                 t.__exit__()

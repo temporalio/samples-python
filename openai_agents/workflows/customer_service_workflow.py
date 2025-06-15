136b40a71caafbfdeea2ffcea96aafe95356dbdc
Johann Schleier-Smith <johann.schleiersmith@temporal.io>
2025-06-14 19:50:03 -0700

    lint fixes

--- openai_agents/workflows/customer_service_workflow.py
+++ openai_agents/workflows/customer_service_workflow.py
@@ -148,15 +148,15 @@ class ProcessUserMessageInput(BaseModel):
 
 @workflow.defn
 class CustomerServiceWorkflow:
-    def __init__(self, input_items: list[TResponseInputItem] = None):
+    def __init__(self, input_items: list[TResponseInputItem] | None = None):
         self.run_config = RunConfig()
-        self.chat_history = []
+        self.chat_history: list[str] = []
         self.current_agent: Agent[AirlineAgentContext] = init_agents()
         self.context = AirlineAgentContext()
         self.input_items = [] if input_items is None else input_items
 
     @workflow.run
-    async def run(self, input_items: list[TResponseInputItem] = None):
+    async def run(self, input_items: list[TResponseInputItem] | None = None):
         await workflow.wait_condition(
             lambda: workflow.info().is_continue_as_new_suggested()
             and workflow.all_handlers_finished()

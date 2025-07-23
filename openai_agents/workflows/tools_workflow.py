db6ccdd4e5ae5cbb175068c9f00a8bfab7e2aad9
Johann Schleier-Smith <johann.schleiersmith@temporal.io>
2025-07-22 20:03:12 -0700

    formatting

--- openai_agents/workflows/tools_workflow.py
+++ openai_agents/workflows/tools_workflow.py
@@ -2,7 +2,7 @@ from __future__ import annotations
 
 from datetime import timedelta
 
-from agents import  trace
+from agents import trace
 from agents import Agent, Runner
 from temporalio import workflow
 from temporalio.contrib import openai_agents as temporal_agents

cb695e024651a8248d94a2e44e150067e44a03b7
Dan Davison <dandavison7@gmail.com>
2025-08-09 10:01:38 -0400

    Add nexus service

--- mcp_sequential_thinking/README.md
+++ mcp_sequential_thinking/README.md
@@ -3,3 +3,22 @@ This sample demonstrates an approach to writing an AI agent workflow that uses t
 - The agent is a Temporal workflow (a durable agent)
 - The sequential thinking MCP server is stateful, and hence is also implemented as a Temporal workflow (a durable MCP server)
 - The durable agent communicates with the durable MCP server via nexus (could also be done via activities, but nexus exists for this purpose)
+
+## Setup
+
+```
+temporal operator nexus endpoint create \
+  --target-namespace default \
+  --name mcp-sequential-thinking-nexus-endpoint \
+  --target-task-queue mcp-sequential-thinking-task-queue
+```
+
+In one terminal, run the Temporal worker in the handler namespace:
+```
+uv run mcp_sequential_thinking/worker.py
+```
+
+In another terminal, start the caller workflow:
+```
+uv run mcp_sequential_thinking/app.py
+```
---
+++ mcp_sequential_thinking/mcp_server/nexus_service.py
@@ -0,0 +1,65 @@
+import uuid
+from dataclasses import dataclass
+
+import nexusrpc
+from mcp.types import (
+    CallToolRequest,
+    CallToolResult,
+    ListToolsRequest,
+    ListToolsResult,
+)
+from temporalio import nexus
+
+
+@dataclass
+class MCPServerStartInput:
+    mcp_server_workflow_name: str
+
+
+@dataclass
+class ListToolsInput:
+    operation_token: str
+    request: ListToolsRequest
+
+
+@dataclass
+class CallToolInput:
+    operation_token: str
+    request: CallToolRequest
+
+
+@nexusrpc.service
+class MCPServerNexusService:
+    start: nexusrpc.Operation[MCPServerStartInput, None]
+    list_tools: nexusrpc.Operation[ListToolsInput, ListToolsResult]
+    call_tool: nexusrpc.Operation[CallToolInput, CallToolResult]
+
+
+@nexusrpc.handler.service_handler(service=MCPServerNexusService)
+class MCPServerNexusServiceHandler:
+    @nexus.workflow_run_operation
+    async def start(
+        self, ctx: nexus.WorkflowRunOperationContext, input: MCPServerStartInput
+    ) -> nexus.WorkflowHandle[None]:
+        return await ctx.start_workflow(
+            input.mcp_server_workflow_name,
+            id=str(uuid.uuid4()),
+        )
+
+    @nexusrpc.handler.sync_operation
+    async def call_tool(
+        self, ctx: nexusrpc.handler.StartOperationContext, input: CallToolInput
+    ) -> CallToolResult:
+        workflow_handle = nexus.WorkflowHandle.from_token(
+            input.operation_token
+        )._to_client_workflow_handle(nexus.client())
+        return await workflow_handle.execute_update("call_tool", input.request)
+
+    @nexusrpc.handler.sync_operation
+    async def list_tools(
+        self, ctx: nexusrpc.handler.StartOperationContext, input: ListToolsInput
+    ) -> ListToolsResult:
+        workflow_handle = nexus.WorkflowHandle.from_token(
+            input.operation_token
+        )._to_client_workflow_handle(nexus.client())
+        return await workflow_handle.query("list_tools", input.request)

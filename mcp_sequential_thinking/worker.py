200cf489f9838c0f46d04bd0ab40a62d00a72c1a
Dan Davison <dandavison7@gmail.com>
2025-08-11 13:17:55 -0400

    Cleanup

--- mcp_sequential_thinking/worker.py
+++ mcp_sequential_thinking/worker.py
@@ -6,9 +6,6 @@ from temporalio.worker import Worker
 from mcp_sequential_thinking.agent_workflow_nexus_client import (
     AgentWorkflowNexusClient,
 )
-from mcp_sequential_thinking.agent_workflow_nexus_transport import (
-    AgentWorkflowNexusTransport,
-)
 from mcp_sequential_thinking.mcp_server.nexus_service import (
     MCPServerNexusServiceHandler,
 )
@@ -24,7 +21,6 @@ async def main():
         task_queue="mcp-sequential-thinking-task-queue",
         workflows=[
             AgentWorkflowNexusClient,
-            AgentWorkflowNexusTransport,
             SequentialThinkingMCPServerWorkflow,
         ],
         nexus_service_handlers=[MCPServerNexusServiceHandler()],
